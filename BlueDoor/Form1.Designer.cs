4bb0b80834c474e7490169e40fbaf6bed8764c37
邵国际 <752147916@qq.com>
2016-06-07 15:50:23 +0800

    Initial Commit

---
+++ BlueDoor/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace BlueDoor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cbPort = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtState = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnUnlock = new System.Windows.Forms.Button();
+            this.btnLock = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.NicontextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.开锁ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.松锁ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.关于ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.NicontextMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cbPort
+            // 
+            this.cbPort.FormattingEnabled = true;
+            this.cbPort.Location = new System.Drawing.Point(50, 12);
+            this.cbPort.Name = "cbPort";
+            this.cbPort.Size = new System.Drawing.Size(68, 20);
+            this.cbPort.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "串口：";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(135, 12);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(63, 20);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "连接";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtState
+            // 
+            this.txtState.BackColor = System.Drawing.SystemColors.Control;
+            this.txtState.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.txtState.Location = new System.Drawing.Point(0, 128);
+            this.txtState.Name = "txtState";
+            this.txtState.Size = new System.Drawing.Size(284, 21);
+            this.txtState.TabIndex = 3;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(50, 43);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(136, 21);
+            this.txtMessage.TabIndex = 5;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(209, 42);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(63, 20);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(209, 12);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(63, 20);
+            this.btnDisconnect.TabIndex = 7;
+            this.btnDisconnect.Text = "断开";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // btnUnlock
+            // 
+            this.btnUnlock.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnUnlock.Location = new System.Drawing.Point(14, 74);
+            this.btnUnlock.Name = "btnUnlock";
+            this.btnUnlock.Size = new System.Drawing.Size(121, 45);
+            this.btnUnlock.TabIndex = 8;
+            this.btnUnlock.Text = "开锁";
+            this.btnUnlock.UseVisualStyleBackColor = true;
+            this.btnUnlock.Click += new System.EventHandler(this.btnUnlock_Click);
+            // 
+            // btnLock
+            // 
+            this.btnLock.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnLock.Location = new System.Drawing.Point(153, 74);
+            this.btnLock.Name = "btnLock";
+            this.btnLock.Size = new System.Drawing.Size(119, 45);
+            this.btnLock.TabIndex = 9;
+            this.btnLock.Text = "松锁";
+            this.btnLock.UseVisualStyleBackColor = true;
+            this.btnLock.Click += new System.EventHandler(this.btnLock_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.NicontextMenu;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "蓝牙门锁";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // NicontextMenu
+            // 
+            this.NicontextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItem1,
+            this.开锁ToolStripMenuItem,
+            this.松锁ToolStripMenuItem,
+            this.关于ToolStripMenuItem,
+            this.ToolStripMenuItem2});
+            this.NicontextMenu.Name = "NicontextMenu";
+            this.NicontextMenu.Size = new System.Drawing.Size(113, 114);
+            // 
+            // ToolStripMenuItem1
+            // 
+            this.ToolStripMenuItem1.Name = "ToolStripMenuItem1";
+            this.ToolStripMenuItem1.Size = new System.Drawing.Size(112, 22);
+            this.ToolStripMenuItem1.Text = "主界面";
+            this.ToolStripMenuItem1.Click += new System.EventHandler(this.ToolStripMenuItem1_Click);
+            // 
+            // ToolStripMenuItem2
+            // 
+            this.ToolStripMenuItem2.Name = "ToolStripMenuItem2";
+            this.ToolStripMenuItem2.Size = new System.Drawing.Size(112, 22);
+            this.ToolStripMenuItem2.Text = "退出";
+            this.ToolStripMenuItem2.Click += new System.EventHandler(this.ToolStripMenuItem2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "消息：";
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 60000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // 开锁ToolStripMenuItem
+            // 
+            this.开锁ToolStripMenuItem.Name = "开锁ToolStripMenuItem";
+            this.开锁ToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.开锁ToolStripMenuItem.Text = "开锁";
+            this.开锁ToolStripMenuItem.Click += new System.EventHandler(this.开锁ToolStripMenuItem_Click);
+            // 
+            // 松锁ToolStripMenuItem
+            // 
+            this.松锁ToolStripMenuItem.Name = "松锁ToolStripMenuItem";
+            this.松锁ToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.松锁ToolStripMenuItem.Text = "松锁";
+            this.松锁ToolStripMenuItem.Click += new System.EventHandler(this.松锁ToolStripMenuItem_Click);
+            // 
+            // 关于ToolStripMenuItem
+            // 
+            this.关于ToolStripMenuItem.Name = "关于ToolStripMenuItem";
+            this.关于ToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.关于ToolStripMenuItem.Text = "关于";
+            this.关于ToolStripMenuItem.Click += new System.EventHandler(this.关于ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(284, 149);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnLock);
+            this.Controls.Add(this.btnUnlock);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtState);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbPort);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "蓝牙门锁";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.NicontextMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtState;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Button btnUnlock;
+        private System.Windows.Forms.Button btnLock;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip NicontextMenu;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.ToolStripMenuItem 开锁ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 松锁ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 关于ToolStripMenuItem;
+    }
+}
+
---
+++ BlueDoor/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// 全局键盘事件监听相关
+using 壁纸管家;
+using System.Runtime.InteropServices;   //调用WINDOWS API函数时要用到
+using Microsoft.Win32;  //写入注册表时要用到
+
+namespace BlueDoor
+{
+    public partial class Form1 : Form
+    {
+        private string Status, BlueToothReceivedData;
+        private KeyboardHook k_hook;
+
+        //Create a serial port for bluetooth
+        private SerialPort BluetoothConnection = new SerialPort();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 全局键盘事件监听相关
+            k_hook = new KeyboardHook();
+            k_hook.KeyDownEvent += new KeyEventHandler(hook_KeyDown);//钩住键按下
+            k_hook.Start();//安装键盘钩子
+
+            //Get all port list for selection
+            //获得所有的端口列表，并显示在列表内
+            cbPort.Items.Clear();
+            string[] Ports = SerialPort.GetPortNames();
+
+            for (int i = 0; i < Ports.Length; i++)
+            {
+                string s = Ports[i].ToUpper();
+                Regex reg = new Regex("[^COM\\d]", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+                s = reg.Replace(s, "");
+
+                cbPort.Items.Add(s);
+            }
+            if (Ports.Length >= 1) cbPort.SelectedIndex = 0;
+        }
+
+        //3.判断输入键值（实现KeyDown事件）
+        private void hook_KeyDown(object sender, KeyEventArgs e)
+        {
+            //判断按下的键（Alt + Z）,开锁
+            if (e.KeyValue == (int)Keys.X && (int)Control.ModifierKeys == (int)Keys.Alt)
+            {
+                //System.Windows.Forms.MessageBox.Show("按下了指定快捷键组合");
+                if (!BluetoothConnection.IsOpen)
+                    bluetoothConnect(); 
+
+                while (!BluetoothConnection.IsOpen) ;
+                Thread.Sleep(500);       
+                BlueToothDataSend("*unlock#");
+                showIconTips("开锁");
+            }
+
+            //判断按下的键（Alt + X）松锁
+            if (e.KeyValue == (int)Keys.C && (int)Control.ModifierKeys == (int)Keys.Alt)
+            {
+                //System.Windows.Forms.MessageBox.Show("按下了指定快捷键组合");
+                if (!BluetoothConnection.IsOpen)
+                    bluetoothConnect();
+
+                while (!BluetoothConnection.IsOpen) ;
+                Thread.Sleep(200);
+                BlueToothDataSend("*lock#");
+                showIconTips("松锁");
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            timer2.Enabled = false;
+            bluetoothConnect();
+        }
+
+        private void BlueToothDataReceived(object o, SerialDataReceivedEventArgs e)
+        {
+            // 无需处理收到的数据
+        }
+
+        private void BlueToothDataSend(string data)
+        {
+            
+            if (BluetoothConnection.IsOpen)
+            {
+                timer2.Enabled = false;  // 关闭自动断开定时器
+                BluetoothConnection.Write(data);  // 发送字符串
+                timer2.Enabled = true;   // 重开定时器
+            }
+            else
+            {
+                Status = "端口被关闭，消息发送失败";
+            }
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            bluetoothDisConnect();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            BlueToothDataSend(txtMessage.Text);
+        }
+
+        private void btnUnlock_Click(object sender, EventArgs e)
+        {
+            BlueToothDataSend("*unlock#");
+        }
+
+        private void btnLock_Click(object sender, EventArgs e)
+        {
+            BlueToothDataSend("*lock#");
+        }
+
+        private void bluetoothConnect()
+        {
+            if (!BluetoothConnection.IsOpen)
+            {
+                //Start
+
+                Status = "正在连接蓝牙设备";
+                showIconTips("正在连接蓝牙设备");
+                BluetoothConnection = new SerialPort();
+                btnConnect.Enabled = false;
+                BluetoothConnection.PortName = cbPort.SelectedItem.ToString();
+                BluetoothConnection.Open();
+                BluetoothConnection.ReadTimeout = 10000;
+                BluetoothConnection.DataReceived += new SerialDataReceivedEventHandler(BlueToothDataReceived);
+                Status = "蓝牙连接成功";
+                showIconTips("蓝牙连接成功");
+            }
+        }
+
+        private void bluetoothDisConnect()
+        {
+            //Stop
+            Status = "正在断开蓝牙设备";
+            showIconTips("正在断开蓝牙设备");
+            BluetoothConnection.Close();
+            //BluetoothConnection.Dispose();
+            //BluetoothConnection = null;
+            btnConnect.Enabled = true;
+            Status = "蓝牙断开成功";
+            showIconTips("蓝牙断开成功");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+            this.Visible = false;
+            this.notifyIcon1.Visible = true;
+            showIconTips("锁锁还在后台运行哦");  // 托盘图标显示消息
+            e.Cancel = true;  // 不关闭程序
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Visible = true;
+                this.ShowInTaskbar = true;  //显示在系统任务栏 
+                this.WindowState = FormWindowState.Normal;  //还原窗体 
+                //this.notifyIcon1.Visible = false;  //托盘图标隐藏 
+            }
+        }
+
+        private void ToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            //点击"是(YES)"退出程序
+            if (MessageBox.Show("确定要退出程序?", "提示",
+                        System.Windows.Forms.MessageBoxButtons.YesNo,
+                        System.Windows.Forms.MessageBoxIcon.Warning)
+                == System.Windows.Forms.DialogResult.Yes)
+            {
+                notifyIcon1.Visible = false;   //设置图标不可见
+                this.Close();                  //关闭窗体
+                this.Dispose();                //释放资源
+                Application.Exit();            //关闭应用程序窗体
+                k_hook.Stop();                 //卸载键盘钩子
+            }
+        }
+
+        private void ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Visible = true;
+                this.ShowInTaskbar = true;  //显示在系统任务栏 
+                this.WindowState = FormWindowState.Normal;  //还原窗体 
+                //this.notifyIcon1.Visible = false;  //托盘图标隐藏 
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            txtState.Text = Status;
+            //txtBlueToothMessage.Text = BlueToothReceivedData;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)  // 自动断开定时器，间隔1分钟
+        {
+            if (BluetoothConnection.IsOpen)
+                bluetoothDisConnect();
+        }
+
+        private void 关于ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("蓝牙门锁电脑控制端-仲恺D446专用！\n" + 
+                            "                                                             ——by邵国际");
+        }
+
+        private void 开锁ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!BluetoothConnection.IsOpen)
+                bluetoothConnect();
+
+            while (!BluetoothConnection.IsOpen) ;
+            Thread.Sleep(500);
+            BlueToothDataSend("*unlock#");
+            showIconTips("开锁");
+        }
+
+        private void 松锁ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!BluetoothConnection.IsOpen)
+                bluetoothConnect();
+
+            while (!BluetoothConnection.IsOpen) ;
+            Thread.Sleep(200);
+            BlueToothDataSend("*lock#");
+            showIconTips("松锁");
+        }
+
+        private void showIconTips(string tip)  // 托盘图标显示消息
+        {
+            if (this.WindowState == FormWindowState.Minimized)  // 窗口最小化
+                notifyIcon1.ShowBalloonTip(100, "提示", tip, ToolTipIcon.None);  // 托盘图标显示消息
+        }
+    }
+}
